5c5f99119d073cc416ec9d5a22ca5efc470d5792
sanket <wankhdes29@gmail.com>
2022-02-08 10:13:53 +0530

    reverse method add in code

--- doublyLinkedList.js
+++ doublyLinkedList.js
@@ -79,4 +79,21 @@ class LinkedList {
     this.length--;
     return this.printList();
   }
+  reverse() {
+    if (!this.head.next) {
+      return this.head;
+    }
+    let first = this.head;
+    this.tail = this.head;
+    let second = first.next;
+    while (second) {
+      const temp = second.nextj;
+      second.next = first;
+      first = second;
+      second = temp;
+    }
+    this.head.next = null;
+    this.head = first;
+    return this;
+  }
 }
